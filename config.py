b302fba4f99d3a21a2b94e847627cfd2f24fd538
kakashiwebx <164495309+kakashiwebx@users.noreply.github.com>
2024-03-28 19:01:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
-API_ID = 123456  # api id
-API_HASH = "ed44780dedd182084f2133b16944cf565"  # api hash
+API_ID = "20791175"  # api id
+API_HASH = "404eae7fd2e3fb85ddeb89bbd4749c9b"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "6761755610:AAHsS43gBQz7tnsr4L8UQszv7jBWGHjo874"  # bot token
 
 
 ## REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
+HOST = "https://kakashiterax-fe8a3bef59f2.herokuapp.com/"  # redis host uri
+PORT =  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002145443746  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
-ADMINS = [1317173146]
+ADMINS = [7133614131]
